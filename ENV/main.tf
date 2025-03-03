f383b404c0e1340c4dd99b9da47f5edca2fda455
careerdeveloper1 <vishal.devops.cd@gmail.com>
2025-03-03 19:00:30 +0530

    rg code

---
+++ ENV/main.tf
@@ -0,0 +1,6 @@
+variable "varenvrg" {}
+
+module "rg-mod" {
+  source = "../RG"
+  varrg = var.varenvrg 
+}
---
+++ RG/main.tf
@@ -0,0 +1,6 @@
+variable "varrg" {}
+resource "azurerm_resource_group" "rg-block" {
+  for_each = var.varrg
+  name     = each.value.rg-name
+  location = each.value.rg-location
+}
